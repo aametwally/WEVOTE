b0b7f1bce5ed0754d423c8016edf363bea8b9c65
Ahmed A. Metwally <ametwa2@uic.edu>
2017-02-17 09:06:44 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+###******************************************************************###
+###********                    WEVOTE                        ********###
+###********     WEighted VOting Taxonomic IdEntification     ********###
+###********                                                  ********###
+###********     Copyright by Ahmed A. Metwally (C) 2016      ********###
+###********        Finn-Perkins' Lab & Dai's Lab, UIC        ********###
+###********                                                  ********###
+###********  This code is released under GNU GPLv3 License   ********###
+###********                                                  ********###
+###********       Please report bugs & suggestions to:       ********###
+###********                 <ametwa2@uic.edu>                ********###
+###******************************************************************###
+
 
 VERSION="1.5.0"
 

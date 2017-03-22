fbc8fd5f5aeb54fe7d3429b99800cb26acde48d0
Ahmed A. Metwally <ametwa2@uic.edu>
2017-03-22 12:25:42 -0500

    Update run_STATISTICS.sh

--- run_STATISTICS.sh
+++ run_STATISTICS.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-## Run the R evaluations cript:
+## Run the R evaluations script:
+# example: ./run_STATISTICS.sh <path-to-AbundanceFiles-dir> <output-prefix>
 module load apps/R-3.2.0
+
 Rscript ./WEVOTE_Statistics.R $1 $2
